e99cba4203ed09f1ef6aa44be7296f455091a2bd
Lugard Agu <agulugard@gmail.com>
2023-03-28 12:22:59 +0100

    Create api key

---
+++ script.js
@@ -0,0 +1,10 @@
+const btnEl = document.getElementById('btn');
+
+const apiKey = bvU7BkqFeNKFrp9aHqDMMg==VIKa9cdnxbzFD63J;
+
+function getJoke() {
+  console.log('clicked');
+} 
+
+
+btnEl.addEventListener('click', getJoke);
