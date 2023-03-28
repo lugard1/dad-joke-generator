da742d11094e2bb6f619a31e0be2a32c3ed7081c
Lugard Agu <agulugard@gmail.com>
2023-03-28 13:29:28 +0100

    Display random jokes and fix linters

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const btnEl = document.getElementById('btn');
+const jokeEl = document.getElementById('joke');
 
-const apiKey = bvU7BkqFeNKFrp9aHqDMMg==VIKa9cdnxbzFD63J;
+const apiUrl = 'https://api.api-ninjas.com/v1/dadjokes?limit=1';
+const apiKey = 'bvU7BkqFeNKFrp9aHqDMMg==VIKa9cdnxbzFD63J';
 
-function getJoke() {
-  console.log('clicked');
-} 
+const options = {
+  method: 'GET',
+  headers: {
+    'X-Api-Key': apiKey,
+  },
+};
 
+async function getJoke() {
+  const response = await fetch(apiUrl, options);
+  const data = await response.json();
+  // console.log(data[0].joke)
+  jokeEl.innerText = data[0].joke;
+}
 
 btnEl.addEventListener('click', getJoke);
